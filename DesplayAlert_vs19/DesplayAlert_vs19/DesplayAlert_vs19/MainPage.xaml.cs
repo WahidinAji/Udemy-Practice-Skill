1fc3be796364a933560cac17ffb5e003011ff017
Wahidin_Aji <a17wahidin@gmail.com>
2019-09-02 21:58:35 +0700

    Access ELement
    mengakses element yang ada pada .xaml di kode belakang .cs

--- DesplayAlert_vs19/DesplayAlert_vs19/DesplayAlert_vs19/MainPage.xaml.cs
+++ DesplayAlert_vs19/DesplayAlert_vs19/DesplayAlert_vs19/MainPage.xaml.cs
@@ -15,7 +15,18 @@ namespace DesplayAlert_vs19
     {
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent(); //inisial ini tidak diambil dari kode xaml nya, 
+                                   //melainkan diambil dari MainPage.xaml.g.cs yang ada di obj->Debug->netstandard2.0
+                                   // private void InitializeComponent() {
+                                   //   global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+                                   // }
+        Content = new Label // dan ini adalah cara untuk menuliskan tampilan secara dinamis melalui kode.cs tetapi ini terlalu panjang dan tidak direkomendasikan
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Text = "Hello World"
+            };
+            
         }
 
         void Button_Clicked(object sender, EventArgs e)
---
+++ DesplayAlert_vs19/SliderAccess/SliderAccess/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SliderAccess
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.8;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            label.Text = string.Format("value is {0:F3}", e.NewValue);
+        }
+    }
+}
