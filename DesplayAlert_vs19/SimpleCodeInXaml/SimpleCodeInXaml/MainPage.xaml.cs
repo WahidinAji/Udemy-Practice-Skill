e465a697c5a9ecf7304ce9e8520fca49c9847a10
Wahidin_Aji <a17wahidin@gmail.com>
2019-09-03 14:10:45 +0700

    Data Binding & Binding Context
    Dalam satu project ada 2 materi, yaitu :
    1. Data Binding
    2. Binding Context
    
    Let's go to move again :boom:

---
+++ DesplayAlert_vs19/SimpleCodeInXaml/SimpleCodeInXaml/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleCodeInXaml
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.3;
+            slider2.Value = 0.6;
+            slider3.Value = 0.9;
+        }
+    }
+}
